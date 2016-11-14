2c03629ece4c92ee92fb322a8595f0bfae1011cc
annniki <nikitina_post@mail.ru>
2016-11-14 20:00:27 +0300

    Add files via upload

---
+++ table_item_create.sql
@@ -0,0 +1,18 @@
+﻿-- Table: items
+
+-- DROP TABLE items;
+
+CREATE TABLE items
+(
+  itemid serial NOT NULL,
+  itemname character varying(255) NOT NULL,
+  itemamount integer NOT NULL,
+  CONSTRAINT items_pkey PRIMARY KEY (itemid),
+  CONSTRAINT items_item_name_key UNIQUE (itemname),
+  CONSTRAINT amount_check CHECK (itemamount > 0)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE items
+  OWNER TO postgres;
